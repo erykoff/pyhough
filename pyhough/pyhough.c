4ab930c4ba6c26409eab082aa8cbc45ebf13df23
Eli Rykoff <esrykoff@lbl.gov>
2013-10-15 13:45:54 -0700

    faster.  Still too slow.

--- pyhough/pyhough.c
+++ pyhough/pyhough.c
@@ -56,11 +56,13 @@ struct hough *hough_new(long *dims,
 }
 
 void _hough_transform(struct hough *self, unsigned short *data) {
-    long i,m,n;
+    //long i,m,n;
+    long i,j,m,n;
     double *costheta,*sintheta;
     long pind;
     long rhoprime;
-    long index;
+    long index,max_index;
+    long *pix_indices;
 
     // precalculate cos/sin
     
@@ -76,9 +78,41 @@ void _hough_transform(struct hough *self, unsigned short *data) {
     for (i=0;i<self->ntheta;i++) {
 	costheta[i] = cos(self->theta[i]);
 	sintheta[i] = sin(self->theta[i]);
-    }    
+    }
 
+    // find the pixels
+    if ((pix_indices = (long *)calloc(self->nrow*self->ncol,sizeof(double))) == NULL) {
+	fprintf(stderr,"Failed to allocate index memory\n");
+	exit(1);
+    }
+
+    index = 0;
+    for (i=0;i<self->nrow*self->ncol;i++) {
+	if (self->image[i]) {
+	    pix_indices[index++] = i;
+	}
+    }
+    max_index = index;
+
+    // loop over good pixels
+    for (i=0;i<self->ntheta;i++) {
+	printf(".");
+	fflush(stdout);
+	for (j=0;j<max_index;j++) {
+	    m = pix_indices[j] % self->ncol;
+	    n = pix_indices[j] / self->ncol;
+
+	    rhoprime = (long) round(m*costheta[i] + n*sintheta[i]);
+	    index = (long) round((rhoprime - self->rho[0])/self->drho);
+	    if ((index >= 0) && (index < self->nrho)) {
+		data[index*self->ntheta + i]++;
+	    }
+	}
+    }
+    printf("\n");
+    
     // loop over each pixel in the theta/rho transform image
+    /*
     for (i=0;i<self->ntheta;i++) {
 	printf(".");
 	fflush(stdout);
@@ -99,10 +133,11 @@ void _hough_transform(struct hough *self, unsigned short *data) {
 	}
     }
     printf("\n");
-
+    */
     // free sin/cos
     free(costheta);
     free(sintheta);
+    free(pix_indices);
 
 }
 
