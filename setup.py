3c2baca5bef5e4359819b9f0f441b2fa59de890f
Eli Rykoff <esrykoff@lbl.gov>
2013-10-17 09:09:14 -0700

    version 0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ ext=Extension("pyhough._pyhough_pywrap",
 
 
 setup(name="pyhough", 
-      version="0.0.3",
+      version="0.1",
       description="Hough transform in python",
       license = "GPL",
       author="Eli Rykoff",
